7fc7f0974bb9fc6f7d7c6066ffa1455956a3e162
Carlos Lawton <carlos.lawton@altisource.com>
2015-01-12 11:03:01 -0500

    import optimization

--- src/android/AmazonMobileAdsPlugin.java
+++ src/android/AmazonMobileAdsPlugin.java
@@ -4,7 +4,6 @@ import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
-import android.widget.LinearLayout;
 import com.amazon.device.ads.*;
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
